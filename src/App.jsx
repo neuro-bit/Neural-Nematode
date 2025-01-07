0c6d8c1255422f9d88d9d7c4de61671fce04172f
neuro <190743852+neuro-bit@users.noreply.github.com>
2025-01-08 00:32:37 +0100

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ConnectomeVisualization from './components/ConnectomeVisualization'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>C. elegans Connectome Visualization</h1>
+      <ConnectomeVisualization />
+    </div>
+  )
+}
+
+export default App
